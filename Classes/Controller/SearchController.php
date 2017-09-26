43db4ed58712ce4d7d1d5aedcb8d4c337288dfd0
Sebastian Schmal <sebastian.schmal@gmx.de>
2017-09-26 16:52:15 +0200

    LICENSE Text überarbeitet

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -6,21 +6,11 @@
  *  (c) 2017 Sebastian Schmal - INGENIUMDESIGN <info@ingeniumdesign.de>
  *  All rights reserved
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This file is part of the "indexed_search" Extension for TYPO3 CMS.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
+ *  For the full copyright and license information, please read the
+ *  LICENSE file that was distributed with this source code.
  *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
  * ************************************************************* */
 
 namespace ID\indexedSearchAutocomplete\Controller;
