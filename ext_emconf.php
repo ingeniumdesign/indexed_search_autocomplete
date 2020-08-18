30e0a0805d7509640445d003a48c5e076a1b3161
felixmaechtle <f.maechtle@ingeniumdesign.de>
2020-08-18 14:29:16 +0200

    fix: fixed ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.0.0-10.9.99',
+            'typo3' => '9.5.0-10.9.99',
         ],
         'conflicts' => [],
     ],
