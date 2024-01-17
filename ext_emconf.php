d024d7406136db438a036d03bb3ebc44480e0f04
Sebastian Schmal <sebastian.schmal@gmx.de>
2024-01-17 19:07:30 +0100

    [TASK] #23: Edit Info Files for Update v11 and v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,13 @@
 <?php
-defined('TYPO3') or die();
+
+/*
+ * This file is part of the package ID\IndexedSearchAutocomplete.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3') or die('Access denied.');
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Indexed Search Autocomplete',
--- ext_tables.php
+++ ext_tables.php
@@ -1,15 +1,19 @@
 <?php
-defined('TYPO3') or die();
+
+/*
+ * This file is part of the package ID\IndexedSearchAutocomplete.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3') or die('Access denied.');
 
 (function(){
-
     // Register Typoscript
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         'indexed_search_autocomplete',
         'Configuration/TypoScript/',
         'Indexed Search AutoComplete'
     );
-
 })();
-
-
