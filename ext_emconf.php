150ae298d7fc8d0d1169c70566cdd96bcddf7bec
Sebastian Schmal <sebastian.schmal@gmx.de>
2025-11-28 15:11:00 +0100

    [UPDATE] #28: Edit the Ext require

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,12 +11,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Indexed Search Autocomplete',
     'description' => 'Extends the TYPO3 Core Extension Indexed_Search searchform with an autocomplete feature.',
     'category' => 'plugin',
+    'version' => '13.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4-13.4.99',
-            'indexed_search' => '12.4-13.4.99',
+            'typo3' => '13.4.0-13.4.99',
+            'indexed_search' => '13.4.0-13.4.99',
+            'php' => '8.2.0-8.4.99',
         ],
         'conflicts' => [],
+        'suggests' => [],
     ],
     'state' => 'stable',
     'uploadfolder' => 0,
@@ -25,5 +28,4 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sebastian Schmal',
     'author_email' => 'info@ingeniumdesign.de',
     'author_company' => 'INGENIUMDESIGN',
-    'version' => '13.0.1',
 ];
